57bb9f9ea70fda4eba11481f508b69ecb6aa420d
Caroline <carolvandrade@gmail.com>
2021-12-21 16:52:01 -0400

    colocando react

---
+++ mega-sena/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MegaSena from "./components/MegaSena/Megasena"
+
+export default function App() {
+    return (
+        <div>
+            <MegaSena></MegaSena>
+        </div>
+    )
+}
--- mega-sena/src/components/MegaSena/Megasena.jsx
+++ mega-sena/src/components/MegaSena/Megasena.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default (props) => {
+export default function MegaSena(props) {
     function gerarNumeroNaoContido(min, max, array) {
         const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min;
         return array.includes(aleatorio) ? 
@@ -17,13 +17,15 @@ export default (props) => {
         .sort((n1, n2) => n1 - n2)
         return numeros
     }
-    
-    const [numeros, setNumeros] = useState(Array(props.qtd || 6).fill(0));
+
+    const qtd = props.qtd || 6
+    const numerosIniciais = Array(qtd).fill(0)
+    const [numeros, setNumeros] = useState(numerosIniciais);
     return (
         <div>
             <h2>MegaSena</h2>
             <h3>{numeros.join(" ")}</h3>
-            <button onClick={_ => setNumeros(gerarNumeros)}>Gerar Números</button>
+            <button onClick={_ => setNumeros(gerarNumeros(qtd))}>Gerar Números</button>
         </div>
     );
 }
