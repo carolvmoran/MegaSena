5ba2f5c20d17dd0ae71e91084f70311271db1ca7
Caroline <carolvandrade@gmail.com>
2021-12-21 15:50:56 -0400

    feat: html inicial

--- Megasena.jsx
+++ Megasena.jsx
@@ -1,18 +1,29 @@
-function gerarNumeroNaoContido(min, max, array) {
-    const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min;
-    return array.includes(aleatorio) ? 
+import React, { useState } from "react";
+
+export default (props) => {
+    function gerarNumeroNaoContido(min, max, array) {
+        const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min;
+        return array.includes(aleatorio) ? 
         gerarNumeroNaoContido(min, max, array) : 
         aleatorio;
-}
- function gerarNumeros(qtd) {
-     const numeros = Array(qtd)
+    }
+    function gerarNumeros(qtd) {
+        const numeros = Array(qtd)
         .fill(0)
         .reduce((nums) => {
             const novoNumero = gerarNumeroNaoContido(1, 60, nums)
             return [ ...nums, novoNumero]
         }, [])
         .sort((n1, n2) => n1 - n2)
-     return numeros
- }
-
- console.log(gerarNumeros(7))
+        return numeros
+    }
+    
+    const [numeros, setNumeros] = useState(Array(props.qtd || 6).fill(0));
+    return (
+        <div>
+            <h2>MegaSena</h2>
+            <h3>{numeros.join(" ")}</h3>
+            <button onClick={_ => setNumeros(gerarNumeros)}>Gerar Números</button>
+        </div>
+    );
+}
